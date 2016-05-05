e8819a49ae72e983909da20f85b386d1f2f31053
Vasily Efimov <Protodwarf@gmail.com>
2016-05-05 21:34:00 +0300

    Menu
    Added dialog menu.

--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
+#include <ctype.h>
 
 using namespace std;
 
@@ -64,11 +65,68 @@ void main(int argc, char **argv)
 
 	//Проверка элементов нетлиста на принадлежность библиотеке
 	if (lib_check())
-		cout << "Netlist contains wrong elements" << endl;
+		cout << endl << "Netlist contains wrong elements" << endl;
 	else
-		cout << "Netlist elements are correnct" << endl;
+		cout << endl << "Netlist elements are correct!" << endl;
 
+	//Interface main loop:
+	while (1)
+	{
+		char com_inp[20];
+		cout << endl << "Command list:" << endl;
+		cout << "1. - \"all\" - List all netlist elements." << endl;
+		cout << "2. - \"typelist\" - List all netlist element types." << endl;
+		cout << "3. - \"type\" - List all elements of choosen type." << endl;
+		cout << "4. - \"info\" - Show element full info." << endl;
+		cout << "5. - \"conn\" - Show elements, connected to choosen one." << endl;
+		cout << "6. - \"rpl\" - Replace element." << endl;
+		cout << "7. - \"typerpl\" - Replace all elements of choosen type." << endl;
+		cout << "8. - \"exit\" - Save and Quit" << endl;
+		cin >> com_inp;
 
-	cout << endl << "Press any key to finish..." << endl;
-	getchar();
+		for (int i = 0; i < 20; i++)
+		{
+			com_inp[i] = tolower(com_inp[i]);
+		}
+
+		if (!strcmp(com_inp, "all"))
+		{
+			cout << "placeholder" << endl;
+		}
+		else if (!strcmp(com_inp, "typelist"))
+		{
+			cout << "placeholder" << endl;
+		}
+		else if (!strcmp(com_inp, "type"))
+		{
+			cout << "placeholder" << endl;
+		}
+		else if (!strcmp(com_inp, "info"))
+		{
+			cout << "placeholder" << endl;
+		}
+		else if (!strcmp(com_inp, "conn"))
+		{
+			cout << "placeholder" << endl;
+		}
+		else if (!strcmp(com_inp, "rpl"))
+		{
+			cout << "placeholder" << endl;
+		}
+		else if (!strcmp(com_inp, "typerpl"))
+		{
+			cout << "placeholder" << endl;
+		}
+		else if (!strcmp(com_inp, "exit"))
+		{
+			cout << endl << "Press any key to finish..." << endl;
+			getchar();
+			getchar();
+			return;
+		}
+		else
+		{
+			cout << "~~~~~Wrong command!~~~~~" << endl;
+		}
+	}
 }
